b663c4e72b8a5aa26d988c473437d4c5ef37b7aa
linguanghui <1476465526@qq.com>
2022-11-23 14:26:02 +0800

    feat: mvp-03

--- dist/node/cli.js
+++ dist/node/cli.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cac_1 = require("cac");
 const dev_1 = require("./dev");
 const path_1 = __importDefault(require("path"));
+const build_1 = require("./build");
 const version = require('../../package.json').version;
 const cli = (0, cac_1.cac)('island').version(version).help();
 cli
@@ -21,6 +22,12 @@ cli
 cli
     .command('build [root]', 'build for production')
     .action(async (root) => {
-    console.log('build', root);
+    try {
+        root = path_1.default.resolve(root);
+        await (0, build_1.build)(root);
+    }
+    catch (e) {
+        console.log(e);
+    }
 });
 cli.parse();
